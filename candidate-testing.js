98187a74ddab3cfea81b9fb4caa34fe67fe52631
bfeig <basketballstpbf@gmail.com>
2021-02-22 03:07:38 +0000

    Updated Code

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,46 +4,40 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
-
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = [
-  'Who was the first American woman in space? ', 
-  'True or false: 5 kilometer == 5000 meters? ', 
-  '(5 + 3)/2 * 10 = ? ', 
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  'Who was the first American woman in space? ',
+  'True or false: 5 kilometer == 5000 meters? ',
+  '(5 + 3)/2 * 10 = ? ',
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
   'What is the minimum crew size for the ISS? '];
 let correctAnswers = [
-  "Sally Ride", 
-  "True", 
-  "40", 
-  "Trajectory", 
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
   "3"];
 let candidateAnswers = [];
 
-
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candinateName = input.question("Please enter your name: ");
-
+candidateName = input.question("Please enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
 for (i = 0; i < questions.length; i ++){
 candidateAnswers[i] = input.question(questions[i]);
-
-  }
 }
 
+}
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  
 for (i = 0; i < questions.length; i ++){
   console.log(`Correct Answer: ${correctAnswers[i]}\nYour Answer: ${candidateAnswers[i]}\n`);
 }
@@ -77,6 +71,7 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
